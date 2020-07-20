3ef840c1fa31fdeae7cf7e49b5dc40b803403c23
DbraunOpus <68357127+DbraunOpus@users.noreply.github.com>
2020-07-20 00:12:37 -0700

    Add files via upload
    This is the user input search for synsets code. It requires the (input.py) program to run, they should be within the same folder. Please use this script for running in the cmd.

---
+++ SynsetsWithInputSearch.py
@@ -0,0 +1,33 @@
+from nltk.corpus import wordnet
+from input import hypernymList
+
+words = []
+#adds to the current list
+words.append(hypernymList)
+syns = {w : [] for w in words}
+#variables
+synsets = 0
+hypernyms = 0
+instance_hypernyms = 0
+
+synsets = synsets+1
+hypernyms = hypernyms+1
+instance_hypernyms = instance_hypernyms+1
+
+instanceCounter=0
+
+while(instanceCounter<2):
+    for k, v in syns.items():
+        for synset in wordnet.synsets(k):
+            synsets+=1
+            for hypernym in synset.hypernyms():
+                hypernyms+=1
+                for lemma in synset.lemmas():
+                        instance_hypernyms+=1
+                        v.append(lemma.name())
+                        m+=1
+                        if (synsets>1):
+                                        print('Found!',synsets,'Synset')
+                        else:
+                                        print('None Found!')
+#this is the code that searches for custom words
