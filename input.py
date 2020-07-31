17ea06f822e9d3693aa39399eb1ce7803976ce6b
DbraunOpus <68357127+DbraunOpus@users.noreply.github.com>
2020-07-31 14:14:06 -0700

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,7 @@
 #this is the input code required for {Synsets.py}
 #keep {Synsets.py and input.py in same folder}
-hypernymInput = input("Type custom hypernyms: ")
+hypernymInput = input("Type custom first word: ")
 hypernymList = hypernymInput
+
+hypernymInput2 = input("Type custom second word: ")
+hypernymList2 = hypernymInput2
